afc5277a7873b9a9e9a65e738316d4dad5c63712
OlgaVysochanska <olavysochanska@gmail.com>
2023-08-09 15:54:44 +0300

    add axios instance, start products

---
+++ src/shared/services/products.js
@@ -0,0 +1,20 @@
+import instance from './instance';
+
+export const getAllProducts = async (page = 1) => {
+  const { data } = await instance.get('/products', {
+    params: {
+      page,
+    },
+  });
+  return data;
+};
+
+export const getProductsByCategory = async (category = 'sets', page = 1) => {
+  const { data } = await instance.get('/products/category', {
+    params: {
+      category,
+      page,
+    },
+  });
+  return data;
+};
