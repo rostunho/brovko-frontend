0af5a522abd846fe36729f6e5bba49c979a36e18
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2023-08-13 00:35:38 +0200

    add routers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { Routes, Route } from 'react-router';
-import AdminPage from 'pages/AdminPage';
-import TempPreview from 'components/Preview/TempPreview';
+import { Routes, Route, Navigate } from 'react-router';
+import { authRoutes, publicRoutes } from './routes';
+
 import './App.css';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<TempPreview />} />
-      <Route path="/admin" element={<AdminPage />} />
+      {/* Public Routes */}
+      {publicRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} element={<Component />} />
+      ))}
+
+      {/* Auth Routes */}
+      {authRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} element={<Component />} />
+      ))}
+
+      {/* Redirect */}
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
---
+++ src/pages/RegisterUserPage/RegisterUserPage.jsx
@@ -0,0 +1,5 @@
+import styles from './RegisterUserPage.module.scss';
+
+export default function RegisterUserPage() {
+  return <>Register user</>;
+}
