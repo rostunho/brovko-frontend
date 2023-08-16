6ef53c14d684ec426f71ae6a8ee0fdc49c2a26f1
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-08-17 01:42:55 +0300

    fix for work

--- src/shared/components/Image/Image.jsx
+++ src/shared/components/Image/Image.jsx
@@ -1,11 +1,14 @@
 import styles from './image.module.scss';
 
+import defaultImage from './defaultImage.png';
+
 const Image = props => {
   const {
-    imageURL = defaultImage,
+    imageURL,
     imageAlt = 'Смаколик',
     imageHeight = '100%',
     imageWidth = '100%',
+    imageClassName,
     imageStyle,
   } = props;
   const imageStyles = {
@@ -13,9 +16,9 @@ const Image = props => {
   };
   return (
     <img
-      className={styles}
+      className={imageClassName || styles.img}
       style={imageStyles}
-      src={imageURL}
+      src={imageURL || defaultImage}
       alt={imageAlt}
       height={imageHeight}
       width={imageWidth}
