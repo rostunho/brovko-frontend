b07595b6d6a8d87422addb6b259cc2806c0b6791
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2023-08-16 15:36:01 +0200

    fix MobileMenu

--- src/components/Navigation/Nav/Nav.jsx
+++ src/components/Navigation/Nav/Nav.jsx
@@ -6,19 +6,24 @@ export default function Nav({ onClick }) {
     <nav>
       <ul className={styles.list}>
         <li className={styles.item} onClick={onClick}>
+          <NavLink className={styles.link} to="/main">
+            Крамничка
+          </NavLink>
+        </li>
+        <li className={styles.item} onClick={onClick}>
+          <NavLink className={styles.link} to="/were-to-buy">
+            Де придбати
+          </NavLink>
+        </li>
+        <li className={styles.item} onClick={onClick}>
           <NavLink className={styles.link} to="/about">
             Про нас
           </NavLink>
         </li>
 
         <li className={styles.item} onClick={onClick}>
-          <NavLink className={styles.link} to="/main">
-            Крамничка
-          </NavLink>
-        </li>
-        <li className={styles.item} onClick={onClick}>
-          <NavLink className={styles.link} to="/admin">
-            ADMIN
+          <NavLink className={styles.link} to="/contacts">
+            КОНТАКТИ
           </NavLink>
         </li>
       </ul>
---
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -0,0 +1,4 @@
+import Contacts from 'components/Contacts/Contacts';
+export default function ContactsPage() {
+  return <Contacts />;
+}
