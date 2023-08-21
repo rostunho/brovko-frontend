a812b988593514cdc88b01e077ecf928d87e8218
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-08-22 01:16:29 +0300

    test test

---
+++ src/components/WeInInstagram/TestInsta.js
@@ -0,0 +1,11 @@
+import html from './testInsta.txt';
+
+const TestInsta = () => {
+  // const embeddedCode = `https://www.instagram.com/p/CvO6VVONa34/`
+
+  return;
+  // <div dangerouslySetInnerHTML={{ __html: embeddedCode }} /
+ ;
+};
+
+export default TestInsta;
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,14 +1,15 @@
 import Hero from 'components/Hero/Hero';
 import WeInInstagram from 'components/WeInInstagram';
 
+import TestInsta from '../../components/WeInInstagram/TestInsta';
 // import styles from './MainPage.scss';
 
 export default function MainPage() {
   return (
     <>
       <Hero />
-      <WeInInstagram />
-      
+      {/* <WeInInstagram /> */}
+      <TestInsta />
     </>
   );
 }
