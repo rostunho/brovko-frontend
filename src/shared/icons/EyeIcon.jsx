de9191d18badcf44dc6064949c9c7669a0bd93ef
Rostyk Podolets <ros.podolets@gmail.com>
2023-08-25 19:12:53 +0300

    All console Warnings are cleared

--- src/shared/icons/EyeIcon.jsx
+++ src/shared/icons/EyeIcon.jsx
@@ -23,16 +23,16 @@ export default function EyeIcon(props) {
           d="M12 5.52C4.5 5.52 1.5 12 1.5 12C1.5 12 4.5 18.48 12 18.48C19.5 18.48 22.5 12 22.5 12C22.5 12 19.5 5.52 12 5.52Z"
           //   stroke="#191C2C"
           stroke="inherit"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M12 14.88C13.6569 14.88 15 13.5906 15 12C15 10.4094 13.6569 9.12 12 9.12C10.3431 9.12 9 10.4094 9 12C9 13.5906 10.3431 14.88 12 14.88Z"
           stroke="inherit"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
     </svg>
