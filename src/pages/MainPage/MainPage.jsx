f6781d28d2d75e0e1f6ca764ae41a7fd08d3166d
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-08-30 14:11:23 +0300

    add loader

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,11 +2,13 @@ import Hero from 'components/Hero/Hero';
 import Perevagy from 'components/Perevagy/Perevagy';
 import Swiper from 'components/Swiper/Swiper';
 import Rectangle from 'components/Rectangle/Rectangle';
+import Loader from 'components/Loader';
 // import styles from './MainPage.scss';
 
 export default function MainPage() {
   return (
     <>
+      <Loader />
       <Hero />
       <Perevagy />
       <Rectangle />
