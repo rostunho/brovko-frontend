1e3bf85d2d5cb26205418a8d90f96c7f978280c4
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-09-02 23:05:27 +0300

    123....

--- src/components/Loader/Loader.stories.jsx
+++ src/components/Loader/Loader.stories.jsx
@@ -1,7 +1,14 @@
-import Loader from './Loader'
+import React from 'react';
+import Loder from './Loader'; // Update the path to your Spiner component
+import './loader.module.scss'; // Update the path to your styles
 
-const meta = {
-    title: 'Components/Loader',
-    component: Loader,
+export default {
+  title: 'Loder', // Set the title for your component
+  component: Loder,
+};
 
-}
+export const Default = () => (
+  <div className="paw">
+    <Loder />
+  </div>
+);
