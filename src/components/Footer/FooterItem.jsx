5a9720ff733b6705398efd3248aa18347105bfd5
Rostyk Podolets <ros.podolets@gmail.com>
2023-09-06 08:52:46 +0300

    Condition for link showing fixed.

--- src/components/Footer/FooterItem.jsx
+++ src/components/Footer/FooterItem.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styles from './Footer.module.scss';
--- src/shared/components/Heading/Heading.jsx
+++ src/shared/components/Heading/Heading.jsx
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import Button from '../Button/Button';
-import classes from './Heading.module.scss';
+import styles from './Heading.module.scss';
 
 export default function Heading({ type = 'h2', withGoBack, children }) {
   const location = useLocation();
@@ -12,19 +12,43 @@ export default function Heading({ type = 'h2', withGoBack, children }) {
   };
 
   return (
-    <div className={classes.container}>
+    <div className={styles.container}>
       {withGoBack && <Button mode="goBack" onClick={onGoBackClick} />}
       {type === 'h1' && (
-        <h1 className={`${classes.heading} ${classes.main}`}>{children}</h1>
+        <h1
+          className={`${styles.heading} ${styles.main} ${
+            withGoBack && styles['with-goback']
+          }`}
+        >
+          {children}
+        </h1>
       )}
       {type === 'h2' && (
-        <h2 className={`${classes.heading} ${classes.large}`}>{children}</h2>
+        <h2
+          className={`${styles.heading} ${styles.large} ${
+            withGoBack && styles['with-goback']
+          }`}
+        >
+          {children}
+        </h2>
       )}
       {type === 'h3' && (
-        <h3 className={`${classes.heading} ${classes.medium}`}>{children}</h3>
+        <h3
+          className={`${styles.heading} ${styles.medium} ${
+            withGoBack && styles['with-goback']
+          }`}
+        >
+          {children}
+        </h3>
       )}
       {type === 'h4' && (
-        <h4 className={`${classes.heading} ${classes.small}`}>{children}</h4>
+        <h4
+          className={`${styles.heading} ${styles.small} ${
+            withGoBack && styles['with-goback']
+          }`}
+        >
+          {children}
+        </h4>
       )}
     </div>
   );
