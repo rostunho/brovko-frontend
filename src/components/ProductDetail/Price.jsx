22c1b3be13b8d24160023338ccc39b0d7a8e5f47
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2023-09-23 15:20:04 +0200

    '/shop/product-list-page'

--- src/components/ProductDetail/Price.jsx
+++ src/components/ProductDetail/Price.jsx
@@ -1,5 +1,6 @@
 import Button from 'shared/components/Button';
 import QuantityButton from 'shared/components/QuantityButton/QuantityButton';
+import QuantityButtonModal from 'shared/components/QuantityButtonModal/QuantityButtonModal';
 
 import styles from './ProductDetail.module.scss';
 
@@ -11,9 +12,13 @@ export default function Price({ price, currencyId }) {
       </h3>
       <div className={styles.amount}>
         {/* <QuantityButton /> */}
-        <Button mode={'adding'} style={{ minWidth: '24px', fill: '#f3a610' }} />
-        <h3>1</h3>
-        <Button mode={'adding'} style={{ minWidth: '24px', fill: '#f3a610' }} />
+        <QuantityButtonModal
+          mode={'outlined'}
+          size={'l'}
+          // style={{ minWidth: '24px', fill: '#f3a610' }}
+        />
+        {/* <h3>1</h3>
+        <Button mode={'adding'} style={{ minWidth: '24px', fill: '#f3a610' }} /> */}
       </div>
     </div>
   );
