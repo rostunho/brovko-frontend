b5b4bc7a33537c8648f29dcdcb2ba7c6c4335fa5
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2023-09-27 21:36:01 +0200

    conflict resolution

--- src/shared/services/instance.js
+++ src/shared/services/instance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_BROVKO_API,
-  // baseURL: 'http://localhost:4999/api',
+  // baseURL: process.env.REACT_APP_BROVKO_API,
+  baseURL: 'http://localhost:5005/api',
 });
 
 export default instance;
