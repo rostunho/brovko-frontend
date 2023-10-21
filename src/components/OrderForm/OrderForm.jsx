6dc42ebb501fa4e1c0235e987bfed63e3963c790
Rostyk Podolets <ros.podolets@gmail.com>
2023-10-21 10:03:53 +0300

    Customer data is delivered to Order Form

--- src/components/OrderForm/OrderForm.jsx
+++ src/components/OrderForm/OrderForm.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import CustomerForm from './CustomerForm/CustomerForm';
 import { DeliveryForm } from 'components/OrderForm/DeliveryForm';
 import PaymentMethod from 'components/OrderForm/PaymentMethod';
 import OrderButtons from './OrderButtons/OrderButtons';
 
 export default function OrderForm() {
+  const [customer, setCustomer] = useState({});
+
+  const getCustomerData = data => {
+    setCustomer(data);
+  };
+
   return (
     <>
-      <CustomerForm />
+      <CustomerForm getData={getCustomerData} />
       <DeliveryForm />
       <PaymentMethod />
       <OrderButtons />
