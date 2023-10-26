37581cbae1805b8f456c6434230c53c5451d7328
OlgaVysochanska <olavysochanska@gmail.com>
2023-10-26 18:21:04 +0300

    almost

--- src/components/UserDashboard/PersonalData/PersonalData.jsx
+++ src/components/UserDashboard/PersonalData/PersonalData.jsx
@@ -1,11 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { current } from 'redux/user/userOperations';
+import { selectUser } from 'redux/user/userSelectors';
 
 import Heading from 'shared/components/Heading';
+import PersonalDataForm from './PersonalDataForm';
 
 import styles from './PersonalData.module.scss';
 
 const PersonalData = () => {
   const [showInfo, setShowInfo] = useState(false);
+
+  const dispatch = useDispatch();
+  const { name, email } = useSelector(selectUser);
+
+  useEffect(() => {
+    dispatch(current());
+  }, [dispatch]);
+
+  const onSubmitForm = data => {
+    console.log(
+      'редагнули особисті дані, треба функція для відправки на бек',
+      data
+    );
+  };
+
+  console.log(name, email);
 
   const toggleShowingInfo = () => {
     setShowInfo(!showInfo);
@@ -15,7 +36,15 @@ const PersonalData = () => {
       <div onClick={toggleShowingInfo} className={styles.heading}>
         <Heading type="h3">Персональні дані</Heading>
       </div>
-      {showInfo && <div className={styles.userInfo}>Блок відкрито</div>}
+      {showInfo && (
+        <div className={styles.userInfo}>
+          <PersonalDataForm
+            firstName={name === undefined ? '' : name}
+            lastName={email === undefined ? '' : email}
+            onSubmitForm={onSubmitForm}
+          />
+        </div>
+      )}
     </>
   );
 };
