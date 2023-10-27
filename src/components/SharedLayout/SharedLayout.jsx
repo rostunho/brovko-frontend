27a0988a17e2160aad0796e11f78f8f32f88901e
Rostyk Podolets <ros.podolets@gmail.com>
2023-10-27 11:17:18 +0300

    Custom hook useScroll is created

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,5 +1,7 @@
-import { Suspense } from 'react';
+import { useState, Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import useScreenWidth from 'shared/hooks/useScreenWidth';
+import useScroll from 'shared/hooks/useScroll';
 
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer';
@@ -10,6 +12,12 @@ import styles from './SharedLayout.module.scss';
 import Loader from 'components/Loader';
 
 const SharedLayout = () => {
+  const screenWidth = useScreenWidth();
+  const scroll = useScroll();
+
+  console.log('screenWidth :>> ', screenWidth);
+  console.log('scroll :>> ', scroll);
+
   return (
     <div className={styles.layout}>
       <Header />
