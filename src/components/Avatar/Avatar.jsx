9c63ef413a2f1a2a8e6a49cc7fc7a5890f3d9e4b
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-11-01 14:24:36 +0200

    add new fix from main

---
+++ src/components/Avatar/Avatar.jsx
@@ -0,0 +1,7 @@
+import Image from "shared/components/Image";
+
+const Avatar = () => {
+  return <Image src='https://www.figma.com/file/wuJFL1lwihdZrrAEeK6OS8/BROVKO-V.2?type=design&node-id=1536-2633&mode=design&t=tJrukVFp2XX6luN8-4' />;
+};
+
+export default Avatar;
