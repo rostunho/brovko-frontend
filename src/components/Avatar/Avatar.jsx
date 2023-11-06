2bb34e15773cbb2242a28b43cf4a682f046c2c40
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-11-06 02:36:48 +0200

    worked

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,16 +1,28 @@
 import Image from 'shared/components/Image';
 
-import styles from './avatar.module.scss';
 import CameraIcon from 'shared/icons/CameraIcon';
+import UserLight from 'shared/icons/UserLight';
+
+import styles from './avatar.module.scss';
+import Button from 'shared/components/Button';
+
 const Avatar = () => {
+  const text='Діана'
   return (
     <>
-      <div className={styles.wrapper}>
-        <Image />
-        <CameraIcon fill='red' />
-      </div>
+    <Button className={styles.wrapper}>
+      {/* <div > */}
+        <Image src="https://shkvarka.ua/wp-content/uploads/05-1-1-e1695742051394.jpg"  className={styles.avatar} text={text} />
+        <CameraIcon className={styles.cameraIcon} 
+        //  fill='var(--white-text-color)' 
+        />
+      {/* </div> */}
+      </ Button>
     </>
   );
 };
 
 export default Avatar;
+
+
+
--- src/shared/components/Image/Image.jsx
+++ src/shared/components/Image/Image.jsx
@@ -3,19 +3,34 @@ import styles from './image.module.scss';
 import defaultImage from './defaultImage.png';
 
 const Image = props => {
-  const { src, alt = 'Смаколик', height, width, className, style } = props;
+  const {
+    src,
+    alt = 'Смаколик',
+    height,
+    width,
+    className,
+    style,
+    text,
+  } = props;
   const imageStyles = {
     ...style,
   };
+  const letterAvatar = text.substring(0, 1).toUpperCase();
+  console.log(letterAvatar);
   return (
-    <img
-      className={className || styles.img}
-      style={imageStyles}
-      src={src || defaultImage}
-      alt={alt}
-      height={height}
-      width={width}
-    />
+    <>
+      {src || <p> className={styles.letter} {letterAvatar} </p> ||
+      
+        <img
+          className={className || styles.img}
+          style={imageStyles}
+          src={src || defaultImage}
+          alt={alt}
+          height={height}
+          width={width}
+        />
+      }
+    </>
   );
 };
 
