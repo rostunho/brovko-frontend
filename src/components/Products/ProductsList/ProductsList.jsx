04e21b8418b7b696a2b701042e472f100b32185d
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2023-11-09 16:26:19 +0100

    fix

--- src/components/Products/ProductsList/ProductsList.jsx
+++ src/components/Products/ProductsList/ProductsList.jsx
@@ -75,6 +75,7 @@ const ProductList = () => {
     }
   }
   console.log('sortedProducts', sortedProducts);
+
   return (
     <div className={styles.products}>
       <SearchBar onSubmit={handleSearchSubmit} />
