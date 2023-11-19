3f279e939b628ce5c5413a4faecc3e09df48c832
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2023-11-20 00:06:46 +0100

    add Rewiew area

--- src/components/ProductDetail/ProductReview/AddReviewForm.jsx
+++ src/components/ProductDetail/ProductReview/AddReviewForm.jsx
@@ -1,7 +1,13 @@
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { fetchAddReview } from 'redux/reviews/reviewsOperations';
+import Button from 'shared/components/Button';
 
-export default function AddReviewForm({ productId, onSubmit }) {
+export default function AddReviewForm({ toggleReviewInput, closeModal }) {
   const [text, setText] = useState('');
+  const { productId } = useParams();
+  const dispatch = useDispatch();
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -10,25 +16,57 @@ export default function AddReviewForm({ productId, onSubmit }) {
       productId,
       text,
     };
-    // Відправка даних на бекенд
-    onSubmit(reviewData);
-    // Скиндання форми після відправки
+
+    dispatch(fetchAddReview(reviewData));
+
     setText('');
+    closeModal();
   };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        {/* <label htmlFor="text">Текст відгуку:</label> */}
-        <textarea
-          placeholder="Напишіть ваш відгук..."
-          id="text"
-          name="text"
-          value={text}
-          onChange={e => setText(e.target.value)}
-          required
-        />
-      </div>
-      <button type="submit">Додати відгук</button>
-    </form>
+    <>
+      <h4>Оцініть, будь ласка, смаколик</h4>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <textarea
+            placeholder="Напишіть ваш відгук..."
+            id="text"
+            name="text"
+            value={text}
+            onChange={e => setText(e.target.value)}
+            required
+          />
+        </div>
+        <Button type="submit" onClick={toggleReviewInput} mode="outlined">
+          Опублікувати
+        </Button>
+      </form>
+    </>
   );
 }
+
+/* <div>
+          <textarea
+            placeholder="Введіть ваш відгук тут..."
+            rows="4"
+            cols="50"
+          />
+          <div style={{ marginTop: 10 }}>
+            {!isAddingPhoto && (
+              <Button type="button" onClick={toggleAddingPhoto} mode="outlined">
+                Додати фото до відгуку
+              </Button>
+            )}
+            {isAddingPhoto && (
+              <Button type="button" mode="filled">
+                Додано фото
+              </Button>
+            )}
+            <Button type="button" onClick={closeReviewInput} mode="outlined">
+              Закрити
+            </Button>
+            <Button type="button" onClick={toggleReviewInput} mode="outlined">
+              Опублікувати
+            </Button>
+          </div>
+        </div> */
--- src/components/ProductDetail/ProductReview/ReviewContainer.jsx
+++ src/components/ProductDetail/ProductReview/ReviewContainer.jsx
@@ -1,21 +1,37 @@
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Button from 'shared/components/Button';
-import AddReviewFormModal from './AddReviewFormModal';
+import AddReviewForm from './AddReviewForm';
 import { ReviewStatistics } from './ReviewStatistics';
 import styles from '../ProductDetail.module.scss';
 
 export default function ReviewContainer() {
   const { productId } = useParams();
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  // const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isReviewInputVisible, setIsReviewInputVisible] = useState(false);
+  const [isAddingPhoto, setIsAddingPhoto] = useState(false);
 
-  const openModal = () => {
-    setIsModalOpen(true);
+  // const openModal = () => {
+  //   setIsModalOpen(true);
+  // };
+
+  // const closeModal = () => {
+  //   setIsModalOpen(false);
+  // };
+
+  const toggleReviewInput = () => {
+    setIsReviewInputVisible(!isReviewInputVisible);
+    setIsAddingPhoto(false); // При відкритті поля для відгуку скидаємо стан додавання фото
   };
 
-  const closeModal = () => {
-    setIsModalOpen(false);
+  const toggleAddingPhoto = () => {
+    setIsAddingPhoto(!isAddingPhoto);
+  };
+
+  const closeReviewInput = () => {
+    setIsReviewInputVisible(false);
+    setIsAddingPhoto(false);
   };
 
   const numberOfReviews = ReviewStatistics({ productId });
@@ -30,18 +46,20 @@ export default function ReviewContainer() {
       <p className={styles.descriptionText}>
         Ваші відгуки допоможуть іншим у виборі смаколика для свого улюбленця!
       </p>
-      <Button
-        type="button"
-        onClick={openModal}
-        mode="outlined"
-        style={{ paddingLeft: 86, paddingRight: 86, marginTop: 20 }}
-      >
-        Залишити відгук
-      </Button>
-      {isModalOpen && (
-        <AddReviewFormModal
-          closeModal={closeModal}
-          isOpen={isModalOpen}
+      {!isReviewInputVisible && (
+        <Button
+          type="button"
+          onClick={toggleReviewInput}
+          mode="outlined"
+          style={{ paddingLeft: 86, paddingRight: 86, marginTop: 20 }}
+        >
+          Залишити відгук
+        </Button>
+      )}
+      {isReviewInputVisible && (
+        <AddReviewForm
+          closeModal={closeReviewInput}
+          isOpen={isReviewInputVisible}
           productId={productId}
         />
       )}
