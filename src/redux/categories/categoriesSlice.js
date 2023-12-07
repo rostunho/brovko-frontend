b61750a77f021ee6d3818f28cc7780cfa4b031ae
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2023-12-07 23:15:14 +0100

    add categories redux
    add categories redux

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { fetchCategories } from './categoriesOperations';
+
+const initialState = {
+  items: [],
+  loading: false,
+  error: null,
+};
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  extraReducers: builder => {
+    builder
+      .addCase(fetchCategories.pending, store => {
+        store.loading = true;
+      })
+      .addCase(fetchCategories.fulfilled, (state, { payload }) => {
+        state.loading = false;
+        state.items = payload.caregory;
+        console.log('state.items___caregory', state.items);
+      })
+      .addCase(fetchCategories.rejected, (store, { payload }) => {
+        store.loading = false;
+        store.error = payload;
+      });
+  },
+});
+
+export default categoriesSlice.reducer;
