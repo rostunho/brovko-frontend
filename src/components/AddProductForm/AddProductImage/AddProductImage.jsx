8c0c11fa0975e35d66cbb088503900dea7317f97
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2024-01-02 01:15:20 +0200

    before global change to use index

--- src/components/AddProductForm/AddProductImage/AddProductImage.jsx
+++ src/components/AddProductForm/AddProductImage/AddProductImage.jsx
@@ -113,7 +113,9 @@ const AddProductImage = ({ pictures }) => {
       .map((file, index) => {
         if (file instanceof Blob) {
           return {
-            id: selectedPictures.length + selectedImages.length + index,
+            id: selectedPictures.length + index,
+
+            // id: selectedPictures.length + selectedImages.length + index,
             url: URL.createObjectURL(file),
           };
         } else {
@@ -128,11 +130,7 @@ const AddProductImage = ({ pictures }) => {
     dispatch(
       addPopupOperation(
         `Додано ${newImages.length} файл${
-          newImages.length === 1
-            ? ``
-            : newImages.length < 5
-            ? `и`
-            : `ів`
+          newImages.length === 1 ? `` : newImages.length < 5 ? `и` : `ів`
         }`
       )
     );
@@ -186,7 +184,7 @@ const AddProductImage = ({ pictures }) => {
       {modalIsOpen && (
         <Modal closeModal={closeModalEditPhoto}>
           <p className={styles.mainText}>
-            {prompDelete
+            {!prompDelete
               ? 'Редагування зображення'
               : 'Ти дійсно бажаєш видалити це фото?'}
           </p>
@@ -205,7 +203,7 @@ const AddProductImage = ({ pictures }) => {
                       setMain(modalIsId);
                     }
                   : () => {
-                      dispatch(addPopupOperation('Головне'));
+                      dispatch(addPopupOperation('Головне фото призначено'));
                     }
                 : () => resetPromp()
             }
@@ -233,131 +231,3 @@ const AddProductImage = ({ pictures }) => {
 };
 
 export default AddProductImage;
-
-// import React, { useState, useEffect } from 'react';
-// import AddIconImage from 'shared/icons/AddIconImage';
-// import styles from './addProductImage.module.scss';
-// import Image from 'shared/components/Image';
-
-// const arraysAreEqual = (arr1, arr2) => {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i].id !== arr2[i].id || arr1[i].url !== arr2[i].url) {
-//       return false;
-//     }
-//   }
-
-//   return true;
-// };
-
-// const AddProductImage = ({ pictures = [] }) => {
-
-//   pictures = [
-//     "https://shkvarka.ua/wp-content/uploads/img_3484-scaled.jpg",
-//     "https://shkvarka.ua/wp-content/uploads/img_3483-scaled.jpg",
-//     "https://shkvarka.ua/wp-content/uploads/img_3477-scaled.jpg",
-//     "https://shkvarka.ua/wp-content/uploads/img_3468-scaled.jpg"
-//   ]
-//   const pictureArray = Array.isArray(pictures) ? pictures : [];
-
-//   const [selectedImages, setSelectedImages] = useState([]);
-//   const [selectedPictures, setSelectedPictures] = useState([]);
-//   const [selectedFiles, setSelectedFiles] = useState([]);
-//   const [prompEdit, setPrompEdit] = useState(false);
-//   const [modalIsOpen, setModalIsOpen] = useState(false);
-
-//   const handleImageChange = (e) => {
-//     e.preventDefault();
-//     const files = Array.from(e.target.files);
-
-//     if (files.length > 0) {
-//       setSelectedFiles(files);
-//     }
-//   };
-
-//   useEffect(() => {
-//     const newImages = selectedFiles.map((file, index) => {
-//       if (file instanceof Blob) {
-//         return {
-//           id: Date.now() + index,
-//           url: URL.createObjectURL(file),
-//         };
-//       } else {
-//         console.error("Invalid file:", file);
-//         return null;
-//       }
-//     }).filter(Boolean);
-
-//     const pictureImages = pictureArray.map((url, index) => ({ id: index, url }));
-
-//     if (!arraysAreEqual([...newImages, ...pictureImages], selectedImages)) {
-//       setSelectedImages([...newImages, ...pictureImages]);
-//     }
-
-//     setSelectedFiles([]);
-//   }, [selectedFiles, pictureArray, selectedImages]);
-
-//   // useEffect(() => {
-//   //   // When pictures prop changes, update selectedImages
-//   //   const newImages = pictureArray.map((url, index) => ({ id: index, url }));
-
-//   //   // Only update the state if newImages is different from selectedImages
-//   //   if (!arraysAreEqual(newImages, selectedImages)) {
-//   //     setSelectedImages(newImages);
-//   //   }
-//   // }, [pictureArray, selectedImages]);
-
-//   const addImages = () => {
-//     if (!selectedFiles.length) {
-//       return;
-//     }
-
-//     const newImages = selectedFiles.map((file, index) => {
-//       if (file instanceof Blob) {
-//         return {
-//           id: Date.now() + index,
-//           url: URL.createObjectURL(file),
-//         };
-//       } else {
-//         console.error("Invalid file:", file);
-//         return null;
-//       }
-//     }).filter(Boolean);
-
-//     setSelectedImages([...selectedImages, ...newImages]);
-//     setSelectedFiles([]);
-//   };
-
-//   return (
-//     <>
-//       <p>Фото товару</p>
-//       <div  className={styles.container}>
-//         {selectedImages.length > 0 &&
-//           selectedImages.map((image) => (
-//             <Image key={image.id} src={image.url} alt={`preview-${image.id}`} className={styles.img} />
-//           ))}
-//         <label className={styles.fileInputLabel}>
-//           <input
-//             className={styles.visuallyHidden}
-//             type="file"
-//             accept="image/jpeg, image/png"
-//             multiple
-//             onChange={(e) => handleImageChange(e)}
-//           />
-//           <AddIconImage />
-//         </label>
-//         <p className={styles.text}>
-//           {selectedImages.length > 0 ? 'Додати ще' : 'Додати фото'}
-//         </p>
-//         <button type="button" onClick={addImages}>
-//           Додати
-//         </button>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default AddProductImage;
