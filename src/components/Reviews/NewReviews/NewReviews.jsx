1c183f70e9f24eddc1fb1dfd7cc87d84d89ad9b5
Rostyk Podolets <ros.podolets@gmail.com>
2024-01-25 17:33:59 +0200

    Reviews moderating page updated

--- src/components/Reviews/NewReviews/NewReviews.jsx
+++ src/components/Reviews/NewReviews/NewReviews.jsx
@@ -1,3 +1,28 @@
+import { useState, useEffect } from 'react';
+import { getReviewsByStatus } from 'shared/services/api/brovko';
+import ReviewItem from 'components/ProductDetail/ProductReview/ReviewItem';
+
 export default function NewReviews({ ...props }) {
-  return <p>Тут будуть нові відгуки</p>;
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await getReviewsByStatus('new');
+      console.log('data :>> ', data);
+      setReviews([...data]);
+    })();
+  }, []);
+
+  return (
+    <ul>
+      {reviews.length > 0 &&
+        reviews.map(review => {
+          return (
+            <li>
+              <ReviewItem review={review} />
+            </li>
+          );
+        })}
+    </ul>
+  );
 }
