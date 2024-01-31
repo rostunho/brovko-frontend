dc21b556469ceff8ce3940f7c4ce3e67483ca09b
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2024-02-01 00:06:27 +0100

    add styles

--- src/components/WhereToBuy/WhereToBuy.jsx
+++ src/components/WhereToBuy/WhereToBuy.jsx
@@ -1,4 +1,6 @@
 import { locationPoints } from './locationPoints';
+import BabayDog from 'shared/icons/BabyDog';
+import Steak from 'shared/icons/Steak';
 import LocationIcon from 'shared/icons/LocationIcon';
 import PhoneIcon from 'shared/icons/PhoneIcon';
 import CalendarIcon from 'shared/icons/CalendarIcon';
@@ -7,36 +9,43 @@ import styles from './WhereToBuy.module.scss';
 export default function WhereToBuy() {
   return (
     <div className={styles.container}>
-  
       <ul className={styles.box}>
-        {locationPoints.map(({ name, city, address, mapLink, phoneNumber, workingHours }, index) => (
+        {locationPoints.map(({ name, foolName, city, address, mapLink, phoneNumber, alternatePhoneNumber, workingHours }, index) => (
           <li className={styles.link} key={index}>
             <div className={styles.card}>
               <div className={styles.icon}>
-                <h3>{name}</h3>
+              <Steak/>
+                <h3 style={{color: '#fefefe'}}>{name}</h3>
               </div>
+              <div className={styles.content}>
+              <div className={styles.text}>{foolName}</div>
               <div className={styles.text}>
-              <LocationIcon/>  {city} {''}
-                  <a href={mapLink} target="_blank" rel="noreferrer" style={{ fontWeight: '700px', fontSize: '14px' }}>
-                    {address}
-                  </a>
-               
-                <p>
+              <LocationIcon/>  
+              {city} {''}
+              <a href={mapLink} target="_blank" rel="noreferrer" style={{ fontWeight: '700px', fontSize: '14px' }}>
+              {address}
+              </a>
+               </div>
+               <div className={styles.text}>
                 <PhoneIcon/>
-                  Телефон{' '}
                   <a href={`tel:${phoneNumber}`}>{phoneNumber}</a>
-                </p>
-                <p>
+                </div>
+                {alternatePhoneNumber ? <div className={styles.text}>
+                <PhoneIcon fill={"rgba(0, 0, 0, 0)"}/>
+                  <a href={`tel:${alternatePhoneNumber}`}>{alternatePhoneNumber}</a>
+                </div> : ''}
+                <div className={styles.text}>
                 <CalendarIcon style={{fill: "#F3A610"}}/>
-                Години роботи:</p>
-                <ul>
+                <ul className={styles.textWorkingHours}>
                   {Object.entries(workingHours).map(([day, hours]) => (
-                    <li className={styles.textWorkingHours} key={day}>
+                    <li  key={day}>
                       {day.charAt(0).toUpperCase() + day.slice(1)}: {hours}
                     </li>
                   ))}
                 </ul>
               </div>
+              </div>
+              
             </div>
           </li>
         ))}
