b7445a73b0c317faff299d0e76492f08275d7e55
Rostyk Podolets <ros.podolets@gmail.com>
2024-02-06 16:01:59 +0200

    Touch hanldle functions are added

--- src/shared/components/ImageBox/ImageBox.jsx
+++ src/shared/components/ImageBox/ImageBox.jsx
@@ -4,6 +4,22 @@ import styles from './ImageBox.module.scss';
 
 export default function ImageBox({ images = [], className }) {
   const [currentImgIdx, setCurrentImgIdx] = useState(0);
+  const [startX, setStartX] = useState(null);
+
+  const handleTouchStart = event => {
+    setStartX(event.changedTouches[0].clientX);
+  };
+
+  const handleTouchEnd = event => {
+    const endX = event.changedTouches[0].clientX;
+    const deltaX = endX - startX;
+
+    if (deltaX > 50) {
+      console.log('Свайп вправо');
+    } else if (deltaX < -50) {
+      console.log('Свайп вліво');
+    }
+  };
 
   return (
     <div className={`${styles['box-container']} ${className ? className : ''}`}>
@@ -12,10 +28,15 @@ export default function ImageBox({ images = [], className }) {
         className={styles['large-image']}
         alt="Смаколик"
       />
-      <div className={styles['mini-gallery']}>
+      <div
+        className={styles['mini-gallery']}
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
+      >
         {images.map((image, idx) => {
           return (
             <img
+              key={idx}
               className={`${styles['small-image']} ${
                 currentImgIdx === idx ? styles.active : 's'
               }`}
