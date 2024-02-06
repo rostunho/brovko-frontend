0aaf669245c71694ede39d577d62b7a4fce3d73e
Rostyk Podolets <ros.podolets@gmail.com>
2024-02-06 11:12:15 +0200

    Product delivered to Page Component

--- src/pages/NewProductDetailPage/NewProductDetailPage.jsx
+++ src/pages/NewProductDetailPage/NewProductDetailPage.jsx
@@ -1,15 +1,38 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { getProductById } from 'shared/services/api';
+import { addPopupOperation } from 'redux/popup/popupOperations';
 
 import Heading from 'shared/components/Heading';
 
 export default function NewProductDetailPage() {
   const { productId } = useParams();
+  const [product, setProduct] = useState(null);
+  const dispatch = useDispatch();
 
-  console.log('productId :>> ', productId);
+  useEffect(() => {
+    if (!productId) {
+      return;
+    }
+
+    getCurrentProduct(productId);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [productId]);
+
+  const getCurrentProduct = async id => {
+    try {
+      const currentProduct = await getProductById(id);
+      setProduct(currentProduct);
+    } catch (error) {
+      console.error(error);
+      dispatch(addPopupOperation('Не вдалося завантажити продукт', error));
+    }
+  };
+
   return (
     <>
-      <Heading withGoBack>Тут буде продукд з дуже довгою назвою</Heading>
+      <Heading withGoBack>{product.name}</Heading>
     </>
   );
 }
