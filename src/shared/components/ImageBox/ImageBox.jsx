6ed6a2cea4930eb546c0e588d1b55edd69e8cf0c
Rostyk Podolets <ros.podolets@gmail.com>
2024-02-07 07:11:26 +0200

    Large image in new slider responsive now

--- src/shared/components/ImageBox/ImageBox.jsx
+++ src/shared/components/ImageBox/ImageBox.jsx
@@ -5,6 +5,7 @@ import styles from './ImageBox.module.scss';
 export default function ImageBox({ images = [], className }) {
   const [currentImgIdx, setCurrentImgIdx] = useState(0);
   const [startX, setStartX] = useState(null);
+  const [offsetX, setOffsetX] = useState(0);
 
   const handleTouchStart = event => {
     setStartX(event.changedTouches[0].clientX);
@@ -14,10 +15,16 @@ export default function ImageBox({ images = [], className }) {
     const endX = event.changedTouches[0].clientX;
     const deltaX = endX - startX;
 
+    // (deltaX > 50 || deltaX < -50) && setOffsetX(deltaX);
+
     if (deltaX > 50) {
       console.log('Свайп вправо');
+      setOffsetX(prevOffset => prevOffset + 1);
+      //   setCurrentImgIdx(prevIdx => prevIdx + 1);
     } else if (deltaX < -50) {
       console.log('Свайп вліво');
+      setOffsetX(prevOffset => prevOffset - 1);
+      //   setCurrentImgIdx(prevIdx => prevIdx - 1);
     }
   };
 
@@ -32,6 +39,7 @@ export default function ImageBox({ images = [], className }) {
         className={styles['mini-gallery']}
         onTouchStart={handleTouchStart}
         onTouchEnd={handleTouchEnd}
+        style={{ transform: `translateX(${offsetX * 80 * 2}px)` }}
       >
         {images.map((image, idx) => {
           return (
