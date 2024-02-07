a4e6130817db8870c6525139b1f4b32f8e8846bc
Rostyk Podolets <ros.podolets@gmail.com>
2024-02-07 10:40:43 +0200

    Bumpers of new Swiper Created

--- src/shared/components/ImageBox/ImageBox.jsx
+++ src/shared/components/ImageBox/ImageBox.jsx
@@ -1,11 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import useScreenWidth from 'shared/hooks/useScreenWidth';
 
 import styles from './ImageBox.module.scss';
 
 export default function ImageBox({ images = [], className }) {
   const [currentImgIdx, setCurrentImgIdx] = useState(0);
   const [startX, setStartX] = useState(null);
+  const [deltaX, setDeltaX] = useState(0);
   const [offsetX, setOffsetX] = useState(0);
+  const [miniGalleryWidth, setMiniGalleryWidth] = useState(0);
+  const screenWidth = useScreenWidth();
+  const [containerWidth, setContainerWidth] = useState(() => screenWidth - 32);
+  const [maxOffset, setMaxOffset] = useState(0);
+
+  console.log('-maxOffset :>> ', -maxOffset);
+
+  useEffect(() => {
+    if (images.length <= 0) {
+      return;
+    }
+
+    setMiniGalleryWidth(images.length * (80 + 12) - 12);
+  }, [images]);
+
+  useEffect(() => {
+    setContainerWidth(screenWidth - 32);
+  }, [screenWidth]);
+
+  useEffect(() => {
+    setMaxOffset(miniGalleryWidth - containerWidth);
+  }, [containerWidth, miniGalleryWidth]);
 
   const handleTouchStart = event => {
     setStartX(event.changedTouches[0].clientX);
@@ -15,15 +39,31 @@ export default function ImageBox({ images = [], className }) {
     const endX = event.changedTouches[0].clientX;
     const deltaX = endX - startX;
 
+    setDeltaX(deltaX);
+
     // (deltaX > 50 || deltaX < -50) && setOffsetX(deltaX);
 
-    if (deltaX > 50) {
+    if (deltaX > 0) {
       console.log('Свайп вправо');
-      setOffsetX(prevOffset => prevOffset + 1);
+      // не реагуємо, якщо галерея в стартовому положені
+      if (offsetX >= 0) {
+        return;
+      }
+
+      deltaX > maxOffset
+        ? setOffsetX(0)
+        : setOffsetX(prevOffset => prevOffset + deltaX);
       //   setCurrentImgIdx(prevIdx => prevIdx + 1);
-    } else if (deltaX < -50) {
+    } else if (deltaX < 0) {
       console.log('Свайп вліво');
-      setOffsetX(prevOffset => prevOffset - 1);
+      // не реагуємо, якщо загальне відхилення вліво вже не менше максимально допустимого відхилення
+      if (offsetX <= -maxOffset) {
+        return;
+      }
+
+      deltaX < -maxOffset
+        ? setOffsetX(-maxOffset)
+        : setOffsetX(prevOffset => prevOffset - -deltaX);
       //   setCurrentImgIdx(prevIdx => prevIdx - 1);
     }
   };
@@ -39,7 +79,7 @@ export default function ImageBox({ images = [], className }) {
         className={styles['mini-gallery']}
         onTouchStart={handleTouchStart}
         onTouchEnd={handleTouchEnd}
-        style={{ transform: `translateX(${offsetX * 80 * 2}px)` }}
+        style={{ transform: `translateX(${offsetX}px)` }}
       >
         {images.map((image, idx) => {
           return (
