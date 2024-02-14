91dbda193925652c78713d829aacc3d1527b24cc
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2024-02-14 03:04:40 +0200

    finish

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -7,6 +7,7 @@ import smakolyk4 from './img/4.png';
 import smakolyk5 from './img/5.png';
 import smakolyk6 from './img/6.png';
 import Text from 'shared/components/Text/Text';
+import { useEffect, useState } from 'react';
 
 export default function NotFound() {
   const images = [
@@ -17,19 +18,46 @@ export default function NotFound() {
     smakolyk5,
     smakolyk6,
   ];
-  const ramdomIndex = Math.floor(Math.random() * images.length);
-  const ramdomImage = images[ramdomIndex];
-  console.log(ramdomImage);
+  const [image, setImage] = useState();
+
+  const generateRamdomImage = () => {
+    const ramdomIndex = Math.floor(Math.random() * images.length);
+    const ramdomImage = images[ramdomIndex];
+    if (image !== ramdomImage) {
+      setImage(ramdomImage);
+    } else {
+      generateRamdomImage();
+    }
+  };
+
+  useEffect(() => {
+    generateRamdomImage();
+  }, []);
+
+  const handleClick = e => {
+    e.preventDefault();
+    generateRamdomImage();
+  };
+  console.log(image);
   return (
     <>
       <div className={styles.wrapper}>
         <p className={styles.textTop}>4</p>
         <div className={styles.spinWrapper}>
-          <Image src={ramdomImage} className={styles.spinning} />
+          <a
+            href=""
+            className={styles.randomLink}
+            onClick={e => handleClick(e)}
+          >
+            <Image src={image} className={styles.spinning} />
+          </a>
         </div>
         <p className={styles.textBottom}>4</p>
       </div>
-      <Text className={styles.message}>Смаколик не знайдено</Text>
+      <Text className={styles.message}>Смаколик не знайдено🤔</Text>
+      <Text className={styles.message}>
+        Клікни на обертаючийся смаколик, щоб отримати інший
+      </Text>
     </>
   );
 }
