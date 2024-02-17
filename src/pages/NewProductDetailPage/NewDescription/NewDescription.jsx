9d2ce4d74459cbfdfab337e2f98b34942d7a1054
Rostyslav <107403659+28Rostyk@users.noreply.github.com>
2024-02-17 10:53:54 +0200

    add format to description

--- src/pages/NewProductDetailPage/NewDescription/NewDescription.jsx
+++ src/pages/NewProductDetailPage/NewDescription/NewDescription.jsx
@@ -1,4 +1,81 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+// import { useSearchParams } from 'react-router-dom';
+// import ReadMoreButton from '../ReadMoreButton/ReadMoreButton';
+// import styles from './NewDescription.module.scss';
+
+// export default function NewDescription({
+//   children,
+//   className,
+//   isMobile,
+//   ...props
+// }) {
+//   const [searchParams, setSearchParams] = useSearchParams();
+//   const descParam = searchParams.get('desc');
+//   const [firstSentence, setFirstSentence] = useState('');
+
+//   useEffect(() => {
+//     isMobile
+//       ? setInitialDescSearchParam('part')
+//       : setInitialDescSearchParam('full');
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [searchParams]);
+
+//   useEffect(() => {
+//     children && extractFirstSentence(children);
+//   }, [children]);
+
+//   const extractFirstSentence = text => {
+//     const targetText = text.match(/.*?[.!?](?:\s|$)/);
+
+//     if (targetText) {
+//       setFirstSentence(targetText);
+//     } else {
+//       setFirstSentence(text);
+//     }
+//   };
+
+//   const setInitialDescSearchParam = value => {
+//     const existingDesc = descParam;
+//     const existingSearchParams = Object.fromEntries(searchParams.entries());
+
+//     existingDesc
+//       ? setSearchParams({ ...existingSearchParams, desc: existingDesc })
+//       : setSearchParams({ ...existingSearchParams, desc: value });
+//   };
+
+//   const handleViewMode = () => {
+//     setSearchParams(prevSearchParams => {
+//       const prevMode = prevSearchParams.get('desc');
+
+//       prevMode === 'full'
+//         ? prevSearchParams.set('desc', 'part')
+//         : prevSearchParams.set('desc', 'full');
+
+//       return prevSearchParams;
+//     });
+//   };
+
+//   return (
+//     <div className={`${styles.container} ${className ? className : ''}`}>
+//       <h3 className={styles.title}>Опис:</h3>
+//       {descParam === 'full' ? (
+//         <p className={styles.text}>{children}</p>
+//       ) : (
+//         <p className={styles.text}>{firstSentence}</p>
+//       )}
+//       {isMobile && (
+//         <ReadMoreButton
+//           className={styles['read-more']}
+//           onClick={handleViewMode}
+//         >
+//           {descParam === 'full' ? 'Згорнути' : 'Читати повністю'}
+//         </ReadMoreButton>
+//       )}
+//     </div>
+//   );
+// }
+
+import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import ReadMoreButton from '../ReadMoreButton/ReadMoreButton';
 import styles from './NewDescription.module.scss';
@@ -25,7 +102,7 @@ export default function NewDescription({
   }, [children]);
 
   const extractFirstSentence = text => {
-    const targetText = text.match(/[^.]*\./);
+    const targetText = text.match(/.*?[.!?](?:\s|$)/);
 
     if (targetText) {
       setFirstSentence(targetText);
@@ -59,7 +136,14 @@ export default function NewDescription({
     <div className={`${styles.container} ${className ? className : ''}`}>
       <h3 className={styles.title}>Опис:</h3>
       {descParam === 'full' ? (
-        <p className={styles.text}>{children}</p>
+        <div>
+          {children?.split('\n').map((paragraph, index) => (
+            <p key={index} className={styles.paragraph}>
+              <span className={styles.indent}>{paragraph.charAt(0)}</span>
+              {paragraph.substring(1)}
+            </p>
+          ))}
+        </div>
       ) : (
         <p className={styles.text}>{firstSentence}</p>
       )}
