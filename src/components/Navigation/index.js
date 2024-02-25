c4b3d9f8b8220a47c1f8052421550e789a043573
Svitlana Cherniakova <107955029+Svchernn@users.noreply.github.com>
2024-02-25 23:29:56 +0200

    add idx

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,1 +1,6 @@
-export { default } from './Navigation';
+export { default as AdminNav } from './AdminNav';
+export { default as AllUserNav } from './AllUserNav';
+export { default as AuthNav } from './AuthNav';
+export { default as Navigation } from './Navigation';
+export { default as ProductNav } from './ProductNav';
+export { default as UserNav } from './UserNav';
