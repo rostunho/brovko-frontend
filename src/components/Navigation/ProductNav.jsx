535d0ca8e694854730946cdc91cd66e72e551aca
Rostyk Podolets <ros.podolets@gmail.com>
2024-02-28 19:55:22 +0200

    Locations Page updated

--- src/components/Navigation/ProductNav.jsx
+++ src/components/Navigation/ProductNav.jsx
@@ -7,19 +7,27 @@ import styles from './Nav.module.scss';
 export default function ProductsNav({ onClick, isMobile }) {
   const { email } = useSelector(selectUser);
   const location = useLocation();
-  // console.log('location in Menu', location);
+  console.log('location in Menu', location);
 
   return (
     <>
       <ul className={styles.list}>
         <li className={styles.item} onClick={onClick}>
-          <NavLink className={styles.link} to="/shop/product-list-page" state={{ from: location }}>
+          <NavLink
+            className={styles.link}
+            to="/shop/product-list-page"
+            state={{ from: location }}
+          >
             Крамничка
           </NavLink>
         </li>
         {isMobile && (
           <li className={styles.item} onClick={onClick}>
-            <NavLink className={styles.link} to="/shop/favourites" state={{ from: location }}>
+            <NavLink
+              className={styles.link}
+              to="/shop/favourites"
+              state={{ from: location }}
+            >
               Обрані
             </NavLink>
           </li>
