13e563e607848bc2b2b0e9a30081bc2a937f5e8e
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2024-03-04 00:56:20 +0100

    add styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,7 @@ import Foto2 from 'shared/images/FotoAbout2.png';
 import Foto3 from 'shared/images/FotoAbout3.png';
 import Foto4 from 'shared/images/FotoAbout4.png';
 import Foto5 from 'shared/images/FotoAbout5.png';
-import { aboutTextDataDesctop } from './smakolykData';
+import { aboutTextDataDesctop, aboutTextDataMobile } from './smakolykData';
 
 import styles from './About.module.scss';
 
@@ -23,11 +23,7 @@ import styles from './About.module.scss';
 
 const MobileAbout = () => (
   <>
-    <div className={styles.yellowContainerMobile}></div>
     <section>
-      <div className={styles.icon}>
-        <BrovkoIcon />
-      </div>
       <h3>Привіт, бро!</h3>
       <div className={styles.aboutText}>
         <p className={styles.aboutText}>{aboutTextDataDesctop[0]}</p>
@@ -67,54 +63,59 @@ const MobileAbout = () => (
   </>
 );
 
-const DesktopAbout = () => (
-  <>
-    <div className={styles.yellowContainer}></div>
-    <section className={styles.containerTitle}>
-      <div className={styles.containerText}>
-        <h3>Привіт, бро!</h3>
-        <p className={styles.aboutText}>{aboutTextDataDesctop[0]}</p>
-        <p className={styles.aboutText}>{aboutTextDataDesctop[1]}</p>
-      </div>
-      <BrovkoIcon width="285" height="218" />
-    </section>
-    <section className={styles.containerBenefits}>
-      <h3>{aboutTextDataDesctop[2]}</h3>
-      <h3>{aboutTextDataDesctop[3]}</h3>
-      <Image src={frame} style={{ margin: '36px auto 16px auto' }} />
-      <GreenSinusIcon />
-    </section>
-    <section className={styles.containerFoto}>
-      <h3>{aboutTextDataDesctop[4]}</h3>
-      <div className={styles.Foto}>
-        <div className={styles.fotoItem}>
-          <Image src={Foto1} />
-        </div>
-        <div className={styles.fotoItem}>
-          <p className={styles.fotoText}>{aboutTextDataDesctop[5]}</p>
-          <Image src={Foto2} />
-        </div>
-      </div>
-    </section>
-    <section className={styles.containerFoto}>
-      <h3>{aboutTextDataDesctop[6]}</h3>
-      <h3>{aboutTextDataDesctop[7]}</h3>
-      <div className={styles.FotoSlid}>
-        <Image src={Foto3} className={styles.fotoItemSlid} />
-        <Image src={Foto4} className={styles.fotoItemSlid} />
-        <Image src={Foto5} className={styles.fotoItemSlid} />
-      </div>
-    </section>
-  </>
-);
-
 export default function About() {
   const layoutType = useLayoutType();
   const isMobile = layoutType === 'mobile';
 
+  const DesktopAbout = () => (
+    <>
+      <section className={styles.containerTitle}>
+        <div className={styles.containerTitleText}>
+          <p className={styles.titleText} style={{ marginBottom: '16px' }}>
+            Привіт, бро!
+          </p>
+          <p className={styles.itemTitleText}>{aboutTextDataDesctop[0]}</p>
+          <p className={styles.itemTitleText}>{aboutTextDataDesctop[1]}</p>
+        </div>
+      </section>
+      <section className={styles.containerBenefits}>
+        <p className={styles.titleText}>
+          {!isMobile ? aboutTextDataDesctop[2] : aboutTextDataMobile[2]}
+        </p>
+        <p className={styles.titleText}>
+          {!isMobile ? aboutTextDataDesctop[3] : aboutTextDataMobile[3]}
+        </p>
+        <Image src={frame} style={{ margin: '36px auto 16px auto' }} />
+      </section>
+      <GreenSinusMobileIcon />
+
+      <section className={styles.containerFoto}>
+        <p className={styles.titleText}>{aboutTextDataDesctop[4]}</p>
+        <div className={styles.Foto}>
+          <div className={styles.fotoItem}>
+            <Image src={Foto1} />
+          </div>
+          <div className={styles.fotoItem}>
+            <p className={styles.fotoText}>{aboutTextDataDesctop[5]}</p>
+            <Image src={Foto2} />
+          </div>
+        </div>
+      </section>
+      <section className={styles.containerFoto}>
+        <p className={styles.titleText}>{aboutTextDataDesctop[6]}</p>
+        <p className={styles.titleText}>{aboutTextDataDesctop[7]}</p>
+        <div className={styles.FotoSlid}>
+          <Image src={Foto3} className={styles.fotoItemSlid} />
+          <Image src={Foto4} className={styles.fotoItemSlid} />
+          <Image src={Foto5} className={styles.fotoItemSlid} />
+        </div>
+      </section>
+    </>
+  );
+
   return (
     <div className={styles.about}>
-      {isMobile ? <MobileAbout /> : <DesktopAbout />}
+      {isMobile ? <DesktopAbout /> : <DesktopAbout />}
     </div>
   );
 }
