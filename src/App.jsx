623ddd9f3eed70278ae817f4115a60b27a2043ae
Rostyk Podolets <ros.podolets@gmail.com>
2024-04-07 11:54:05 +0300

    Auth control over App  added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import { lazy, Suspense } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
-// import { selectIsLogin } from 'redux/user/userSelectors';
-import { lazy } from 'react';
+import { useSelector } from 'react-redux';
+import { selectIsLogin } from 'redux/user/userSelectors';
 import SharedLayout from 'components/SharedLayout/SharedLayout';
 import AllUsersRoutes from 'components/Routes/AllUsersRoutes';
 import ProductsRoutes from 'components/Routes/ProductsRoutes';
@@ -12,16 +11,7 @@ import PublicRoute from 'utils/Routers/PublicRoute';
 import PrivateRoute from 'utils/Routers/PrivateRoute';
 import AdminRoute from 'utils/Routers/AdminRoute';
 import OrderRoutes from 'components/Routes/OrderRoutes';
-
-// const AuthFormWrapper = lazy(() =>
-//   import('components/AuthSection/AuthFormWrapper/AuthFormWrapper')
-// );
-// const LoginForm = lazy(() =>
-//   import('components/AuthSection/LoginForm/LoginForm')
-// );
-
-// const OrderForm = lazy(() => import('components/OrderForm/OrderForm'));
-// const OrderPage = lazy(() => import('pages/OrderPage'));
+import Loader from 'components/Loader';
 
 const LazyUserDashboardPage = lazy(() =>
   import('pages/UserDashboardPage/UserDashboardPage')
@@ -29,37 +19,43 @@ const LazyUserDashboardPage = lazy(() =>
 const LazyMainPage = lazy(() => import('pages/MainPage/MainPage'));
 const LazyNotFoundPage = lazy(() => import('pages/NotFoundPage/NotFoundPage'));
 const TestingPage = lazy(() => import('pages/Testing/TestingPage'));
+const LoginDemoPage = lazy(() => import('pages/LoginDemoPage/LoginDemoPage'));
 
 function App() {
-  // const userIsLogin = useSelector(selectIsLogin);
-  // console.log('userIsLogin :>> ', userIsLogin);
+  const userIsLogin = useSelector(selectIsLogin);
+  console.log('userIsLogin :>> ', userIsLogin);
 
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<Navigate to="/main" />} />
-        <Route path="/main" element={<LazyMainPage />} />
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route
+          path="/"
+          element={userIsLogin ? <SharedLayout /> : <LoginDemoPage />}
+        >
+          {userIsLogin && <Route index element={<Navigate to="/main" />} />}
+          <Route path="/main" element={<LazyMainPage />} />
 
-        <Route element={<PublicRoute/>}>
-              <Route path="/shop/*" element={<ProductsRoutes />} />
-              <Route path="/all/*" element={<AllUsersRoutes />} />
-              <Route path="/auth/*" element={<AuthRoutes />} /> 
-              <Route path='/order/*' element={<OrderRoutes/>} />
+          <Route element={<PublicRoute />}>
+            <Route path="/shop/*" element={<ProductsRoutes />} />
+            <Route path="/all/*" element={<AllUsersRoutes />} />
+            <Route path="/auth/*" element={<AuthRoutes />} />
+            <Route path="/order/*" element={<OrderRoutes />} />
+          </Route>
+
+          <Route element={<PrivateRoute />}>
+            <Route path="/user" element={<LazyUserDashboardPage />} />
+          </Route>
+
+          <Route element={<AdminRoute />}>
+            <Route path="/admin/*" element={<AdminRoutes />} />
+          </Route>
+
+          <Route path="*" element={<LazyNotFoundPage />} />
+
+          <Route path="/testing" element={<TestingPage />} />
         </Route>
-
-        <Route element={<PrivateRoute />}>
-          <Route path="/user" element={<LazyUserDashboardPage />} />
-        </Route>
-
-        <Route element={<AdminRoute />}>
-          <Route path="/admin/*" element={<AdminRoutes />} />
-        </Route>
-
-        <Route path="*" element={<LazyNotFoundPage />} />
-
-        <Route path="/testing" element={<TestingPage />} />
-      </Route>
-    </Routes>
+      </Routes>
+    </Suspense>
   );
 }
 
